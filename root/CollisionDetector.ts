8bc742b495c99bc830e279889148dbcb0f86e713
Käkäsh <marco.zillgen23@gmail.com>
2022-09-20 08:46:13 +0200

    added Collision interface

--- root/CollisionDetector.ts
+++ root/CollisionDetector.ts
@@ -5,5 +5,45 @@ export default function CollisionDetector (
   thisObject: LogicObject,
   otherObjects: LogicObject[]
 ) {
-  const 
+  const topCollision = otherObjects.filter(
+    otherObject =>
+      thisObject.position.y + thisObject.height >= otherObject.position.y &&
+      thisObject.position.y + thisObject.height <=
+        otherObject.position.y + otherObject.height &&
+      thisObject.position.x + thisObject.width >= otherObject.position.x &&
+      thisObject.position.x <= otherObject.position.x + otherObject.width
+  )
+
+  const bottomCollision = otherObjects.filter(
+    otherObject =>
+      thisObject.position.y <= otherObject.position.y + otherObject.height &&
+      thisObject.position.y >= otherObject.position.y &&
+      thisObject.position.x + thisObject.width >= otherObject.position.x &&
+      thisObject.position.x <= otherObject.position.x + otherObject.width
+  )
+
+  const leftCollision = otherObjects.filter(
+    otherObject =>
+      thisObject.position.x + thisObject.width >= otherObject.position.x &&
+      thisObject.position.x + thisObject.width <=
+        otherObject.position.x + otherObject.width &&
+      thisObject.position.y + thisObject.height >= otherObject.position.y &&
+      thisObject.position.y <= otherObject.position.y + otherObject.height
+  )
+
+  const rightCollision = otherObjects.filter(
+    otherObject =>
+      thisObject.position.x <= otherObject.position.x + otherObject.width &&
+      thisObject.position.x >= otherObject.position.x &&
+      thisObject.position.y + thisObject.height >= otherObject.position.y &&
+      thisObject.position.y <= otherObject.position.y + otherObject.height
+  )
+
+  return {
+    top: topCollision,
+    bottom: bottomCollision,
+    left: leftCollision,
+    right: rightCollision
+  }
 }
+
