d3d463513a44f7d4bd684c9316c04673e3778c4e
Käkäsh <marco.zillgen23@gmail.com>
2022-09-21 18:35:45 +0200

    Collision Folder

--- root/Collision.ts
+++
@@ -1,8 +0,0 @@
-import LogicObject from './logic/LogicObject'
-
-export default interface Collision {
-  top: LogicObject[]
-  bottom: LogicObject[]
-  left: LogicObject[]
-  right: LogicObject[]
-}
